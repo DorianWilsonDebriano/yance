2a997e5e8bb66f0f5c313bad9288beb63e030c3c
Cassandra Dodd <cassy.dodd95@gmail.com>
2020-11-24 19:41:49 +0100

    one more seed added to check filtering from the search, filtering if statement added

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,7 @@ adham = User.create!(
   last_name: "Jaafar",
   bio: "I live, breathe, eat sports",
   language: "English",
-
-  )
+)
 
 cassy = User.create!(
   email: "cassy@gmail.com",
@@ -44,6 +43,14 @@ edoardo = User.create!(
   language: "English",
   )
 
+sarah = User.create!(
+  email: "sarah@gmail.com",
+  password: "sarah123",
+  first_name: "Sarah",
+  last_name: "Smith",
+  bio: "I live, breathe, eat sports",
+  language: "English",
+)
 # TRAINER SEEDS
 
 trainer_1 = Trainer.new(
@@ -51,7 +58,7 @@ trainer_1 = Trainer.new(
   sport_category: "Cardio",
   city: "Berlin",
   user: edoardo,
-    )
+)
 # TRAINER PIC
 trainer_1.save!
 
@@ -60,7 +67,7 @@ trainer_2 = Trainer.new(
   sport_category: "Yoga",
   city: "Paris",
   user: dorian,
-    )
+)
 # TRAINER PIC
 trainer_2.save!
 
@@ -69,7 +76,7 @@ trainer_3 = Trainer.new(
   sport_category: "Zumba",
   city: "Copenhagen",
   user: cassy,
-    )
+)
 # TRAINER PIC
 trainer_3.save!
 
@@ -78,9 +85,18 @@ trainer_4 = Trainer.new(
   sport_category: "HITT",
   city: "Amsterdam",
   user: adham,
-    )
+)
 # TRAINER PIC
 trainer_4.save!
+
+trainer_5 = Trainer.new(
+  bio: "In my classes you will feel the burn!",
+  sport_category: "Yoga",
+  city: "London",
+  user: sarah,
+)
+# TRAINER PIC
+trainer_5.save!
 
 # CLASS SEEDS
 
@@ -153,6 +169,22 @@ sports_class_4 = SportsClass.new(
 # CLASS PIC/VIDEO?
 sports_class_4.save!
 
+sports_class_5 = SportsClass.new(
+    title: "Saturday Start",
+    description: "A smooth start to your stress-free weekend",
+    date_time: DateTime.new(2020,2,6,8),
+    duration: "90 mins",
+    category: "Yoga",
+    difficulty_level: 2,
+    sweat_level: 3,
+    experience_level: "Intermediate",
+    equipment: false,
+    language: "English",
+    trainer: trainer_5,
+)
+
+# CLASS PIC/VIDEO?
+sports_class_5.save!
 
 puts 'new seeds added!'
 
