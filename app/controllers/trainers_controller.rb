3555f7af1512b7422223b5ae78c4c225acbd2d89
Adham Jaafar <ajaafar65@gmail.com>
2020-11-24 11:57:24 +0100

    trainer controller

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,11 +1,27 @@
 class TrainersController < ApplicationController
+before_action :authenticate_user!
 
   def show
+    @trainer = Trainer.find(params[:id])
   end
 
   def new
+    @trainer = Trainer.new
   end
 
   def create
+    @trainer = Trainer.new(trainer_params)
+    @trainer.user = current_user
+    if @trainer.save!
+      redirect_to trainer_path(@trainer)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trainer_params
+    params.require(:trainer).permit(:bio, :city, :sport_category)
   end
 end
