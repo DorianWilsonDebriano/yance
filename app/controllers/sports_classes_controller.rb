833708e22d316ae71d2fb8b9cb0b1e1c71d21734
Dorian Wilson-Debriano <dw.debriano@gmail.com>
2020-11-24 12:41:38 +0100

    new sportsclass

--- app/controllers/sports_classes_controller.rb
+++ app/controllers/sports_classes_controller.rb
@@ -1,4 +1,6 @@
 class SportsClassesController < ApplicationController
+  before_action :set_sports_class
+
   def index
   end
 
@@ -6,9 +8,18 @@ class SportsClassesController < ApplicationController
   end
 
   def new
+    @sportsclass = SportsClass.new
   end
 
   def create
+    @trainer = Trainer.find(params[:trainer_id])
+    @sportsclass = SportsClass.new(sports_class_params)
+    @sportsclass.trainer = @trainer
+    if @sportsclass.save
+      redirect_to @sportsclass, notice: "Your class has been created"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,6 +32,10 @@ class SportsClassesController < ApplicationController
   end
 
   private
+
+  def set_sports_class
+    @sportsclass = SportsClass.find(params[:id])
+  end
 
   def sports_class_params
     params.require(:sports_class).permit(:title, :description, :date_time, :duration, :category, :difficulty_level, :sweat_level, :experience_level, :equipment, :language)
