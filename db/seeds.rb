606b93a9e560a3137478aae7a00d6237fbaaeadb
Cassandra Dodd <cassy.dodd95@gmail.com>
2020-11-29 13:58:28 +0100

    started making seeds more unique and added a temporary height fix for the card headers

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,8 +34,8 @@ cassy = User.new(
   password: "cassy123",
   first_name: "Cassy",
   last_name: "Dodd",
-  bio: "I live, breathe, eat sports",
-  language: "English",
+  bio: "I love to both attend classes on Yance and offer them!",
+  language: "English, German",
   )
 cassy.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
 cassy.save!
@@ -47,7 +47,7 @@ dorian = User.new(
   password: "dorian123",
   first_name: "Dorian",
   last_name: "Wilson",
-  bio: "I live, breathe, eat sports",
+  bio: "Feel free to join my classes, hope to see you there!",
   language: "English",
   )
 dorian.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
@@ -61,7 +61,7 @@ edoardo = User.new(
   first_name: "Edoardo",
   last_name: "Maraia",
   bio: "I live, breathe, eat sports",
-  language: "English",
+  language: "English, Italian",
   )
 edoardo.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
 edoardo.save!
@@ -87,9 +87,9 @@ trainer_2 = Trainer.new(
 trainer_2.save!
 
 trainer_3 = Trainer.new(
-  bio: "Let's dance!",
+  bio: "An inclusive classroom environment where we can shape-up out minds and body is my mantra",
   sport_category: "Zumba",
-  city: "Copenhagen",
+  city: "Frankfurt",
   user: cassy,
 )
 # TRAINER PIC
@@ -105,11 +105,28 @@ trainer_4 = Trainer.new(
 trainer_4.save!
 
 # CLASS SEEDS
-file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606491396/dane-wetton-t1NEMSm1rgI-unsplash_ksd3vg.jpg')
+file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606651900/pexels-cottonbro-4753923_o90hn5.jpg')
 sports_class_1 = SportsClass.new(
+    title: "Kick Start",
+    description: "Let's kick start our way into the coming week right!",
+    date_time: DateTime.new(2020,12,6,18),
+    duration: "60 mins",
+    category: "Kick Boxing",
+    difficulty_level: 2,
+    sweat_level: 3,
+    experience_level: "Intermediate",
+    equipment: true,
+    language: "German",
+    trainer: trainer_3,
+)
+sports_class_1.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_1.save!
+
+file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606491396/dane-wetton-t1NEMSm1rgI-unsplash_ksd3vg.jpg')
+sports_class_2 = SportsClass.new(
     title: "Monday Morning Wake-up",
     description: "Start your week right with this slow-burner!",
-    date_time: DateTime.new(2020,11,23,7),
+    date_time: DateTime.new(2020,12,7,7),
     duration: "50 mins",
     category: "Stretching",
     difficulty_level: 2,
@@ -119,14 +136,14 @@ sports_class_1 = SportsClass.new(
     language: "English",
     trainer: trainer_1,
 )
-sports_class_1.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_1.save!
+sports_class_2.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_2.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606303073/an6ys6litaabmulfs6hmzkz7zajn.jpg')
-sports_class_2 = SportsClass.new(
+sports_class_3 = SportsClass.new(
     title: "Tuesday Flex",
     description: "Lets make the week even better together",
-    date_time: DateTime.new(2020,11,24,17),
+    date_time: DateTime.new(2020,12,8,17),
     duration: "60 mins",
     category: "Yoga",
     difficulty_level: 2,
@@ -137,14 +154,14 @@ sports_class_2 = SportsClass.new(
     trainer: trainer_2,
 )
 
-sports_class_2.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_2.save!
+sports_class_3.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_3.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606487059/raphael-nast-2ia6IXXNGwY-unsplash_qki4wo.jpg')
-sports_class_3 = SportsClass.new(
+sports_class_4 = SportsClass.new(
     title: "Ballet Fit",
     description: "Look and feel like a dancer with Cassy. Her workout consists of ballet and palettes, which will help sculpt and shape your body. You won't be twirling or prancing, but you will feel the burn!",
-    date_time: DateTime.new(2020,12,01,13),
+    date_time: DateTime.new(2020,12,9,13),
     duration: "30 mins",
     category: "dance",
     difficulty_level: 4,
@@ -155,14 +172,14 @@ sports_class_3 = SportsClass.new(
     trainer: trainer_3,
 )
 
-sports_class_3.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_3.save!
+sports_class_4.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_4.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606491474/alexander-redl-d3bYmnZ0ank-unsplash_cp8d5d.jpg')
-sports_class_4 = SportsClass.new(
+sports_class_5 = SportsClass.new(
     title: "Feel it Friday",
     description: "Burn off the weeks stress and frustrations!",
-    date_time: DateTime.new(2020,11,29,18),
+    date_time: DateTime.new(2020,12,11,18),
     duration: "40 mins",
     category: "HITT",
     difficulty_level: 2,
@@ -174,14 +191,14 @@ sports_class_4 = SportsClass.new(
 )
 
 
-sports_class_4.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_4.save!
+sports_class_5.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_5.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606491307/jonathan-borba-lrQPTQs7nQQ-unsplash_rrjtqc.jpg')
-sports_class_5 = SportsClass.new(
+sports_class_6 = SportsClass.new(
     title: "Lunch Break Energizer",
     description: "No time for both lunch break and a workout? Those days are over when you try the new 10-minute version of Adham's popular lunch break workout!",
-    date_time: DateTime.new(2020,11,27,20),
+    date_time: DateTime.new(2020,12,10,20),
     duration: "40 mins",
     category: "HITT",
     difficulty_level: 2,
@@ -193,14 +210,14 @@ sports_class_5 = SportsClass.new(
 )
 
 
-sports_class_5.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_5.save!
+sports_class_6.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_6.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606389633/8zqw2ptcu4sonnqcpdzpp04j0ekl.jpg')
-sports_class_6 = SportsClass.new(
+sports_class_7 = SportsClass.new(
     title: "Pure Pilates",
     description: "Your everyday go-to pilates flow. Perfect for all levels this class is designed to lengthen, align and sculpt. Edoardo will take time in these class to focus on technique and breathing!",
-    date_time: DateTime.new(2020,11,30,15),
+    date_time: DateTime.new(2020,12,12,15),
     duration: "40 mins",
     category: "Pilates",
     difficulty_level: 2,
@@ -212,14 +229,14 @@ sports_class_6 = SportsClass.new(
 )
 
 
-sports_class_6.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_6.save!
+sports_class_7.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_7.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606486727/emily-sea-coiWR0gT8Cw-unsplash_vzy3ry.jpg')
-sports_class_7 = SportsClass.new(
+sports_class_8 = SportsClass.new(
     title: "Yoga Vinyasa",
     description: "Vinyasa flow is a strong and well rounded practice that includes core conditioning, flexibility, balance and movement.",
-    date_time: DateTime.new(2020,12,29,16),
+    date_time: DateTime.new(2020,12,9,16),
     duration: "90 mins",
     category: "Yoga",
     difficulty_level: 2,
@@ -231,11 +248,11 @@ sports_class_7 = SportsClass.new(
 )
 
 
-sports_class_7.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_7.save!
+sports_class_8.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_8.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606490125/taco-fleur-qxoOajPS55U-unsplash_iyiup8.jpg')
-sports_class_8 = SportsClass.new(
+sports_class_9 = SportsClass.new(
     title: "Kettle Bell Crush",
     description: "This is another hard-hitting workout by the kettlebell king. Tyler's workout will define every inch of your body.",
     date_time: DateTime.new(2020,12,30,13),
@@ -250,11 +267,11 @@ sports_class_8 = SportsClass.new(
 )
 
 
-sports_class_8.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_8.save!
+sports_class_9.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_9.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606490631/victor-freitas-WvDYdXDzkhs-unsplash_e9kbsd.jpg')
-sports_class_9 = SportsClass.new(
+sports_class_10 = SportsClass.new(
     title: "Iron Bars",
     description: "There's nothing easy about Adham's workout, but it's well worth it! You'll be doing a whole lot of lifting, deadlift and squats using your barbell Now let's get you summer ready!",
     date_time: DateTime.new(2020,12,5,13),
@@ -269,11 +286,11 @@ sports_class_9 = SportsClass.new(
 )
 
 
-sports_class_9.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_9.save!
+sports_class_10.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_10.save!
 
 file = URI.open('https://res.cloudinary.com/dorianwilsondebriano/image/upload/v1606491710/madison-lavern-4gcqRf3-f2I-unsplash_ljr7uu.jpg')
-sports_class_10 = SportsClass.new(
+sports_class_11 = SportsClass.new(
     title: "Just Relax",
     description: "Sometimes we just need relax and reboot. Come away with us and free your mind of any worries. Dorian's workout is a combination of yoga and stretching which will activate and relax your muscles.",
     date_time: DateTime.new(2020,12,7,20),
@@ -288,7 +305,7 @@ sports_class_10 = SportsClass.new(
 )
 
 
-sports_class_10.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
-sports_class_10.save!
+sports_class_11.photo.attach(io: file, filename: 'profile_pic.jpg', content_type: 'image/jpg')
+sports_class_11.save!
 
 puts 'new seeds planted!'
