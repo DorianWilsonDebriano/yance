4275f42e60e4593a19dacfc6785bb27a60b9a19a
edoardo maraia <edoardo.maraia@gmail.com>
2020-11-30 16:54:17 +0100

    cards filters

--- app/controllers/sports_classes_controller.rb
+++ app/controllers/sports_classes_controller.rb
@@ -1,30 +1,17 @@
 require "date"
-
 class SportsClassesController < ApplicationController
   before_action :set_sports_class, only: [:show, :edit, :update, :destroy]
-
   def index
     @sports_classes = policy_scope(SportsClass).order(date_time: :asc)
-    if params[:query].present?
-      @sports_classes = @sports_classes.search(params[:query])
-    end
-    if params[:starts_at].present?
-      @sports_classes = @sports_classes.filter do |sports_class|
-        time_query = params[:starts_at].split(' ')
-        range_one = time_query.first.to_date
-        range_two = time_query.last.to_date
-        sports_class.date_time.to_date.between?(range_one, range_two)
-
-      end
-    end
-
+    handle_search
+    handle_date_search
+    handle_filters
+    handle_filter_cards
     @classbooking = ClassBooking.new
     @classbookings = policy_scope(ClassBooking).where(user: current_user)
   end
-
   def show
   end
-
   def new
     @trainer = Trainer.find(params[:trainer_id])
     @trainer.user = current_user
@@ -32,7 +19,6 @@ class SportsClassesController < ApplicationController
     authorize @sportsclass
     # authorize @trainer, policy_class: SportsClassPolicy
   end
-
   def create
     @trainer = Trainer.find(params[:trainer_id])
     @sportsclass = SportsClass.new(sports_class_params)
@@ -44,11 +30,9 @@ class SportsClassesController < ApplicationController
       render :new
     end
   end
-
   def edit
     authorize @sportsclass
   end
-
   def update
     authorize @sportsclass
     if @sportsclass.update(sports_class_params)
@@ -57,21 +41,46 @@ class SportsClassesController < ApplicationController
       render :edit
     end
   end
-
   def destroy
     # authorize @sportsclass
     @sportsclass.destroy
     redirect_to sports_classes_path, notice: "#{@sportsclass.title} has been deleted"
   end
-
   private
-
   def set_sports_class
     @sportsclass = SportsClass.find(params[:id])
     authorize @sportsclass
   end
-
   def sports_class_params
     params.require(:sports_class).permit(:title, :description, :date_time, :duration, :category, :difficulty_level, :sweat_level, :experience_level, :equipment, :language, :photo)
+  end
+  def handle_search
+    if params[:query].present?
+      @sports_classes = @sports_classes.search(params[:query])
+    end
+  end
+  def handle_date_search
+    if params[:starts_at].present?
+      @sports_classes = SportsClass.where(date_time: Range.new(*params[:starts_at].split(" to ")))
+    end
+  end
+
+  def handle_filter_cards
+    if params.dig(:sports_class, :category).present?
+      @sports_classes = @sports_classes.where(category: params[:sports_class][:category])
+    end
+  end
+
+  def handle_filters
+    if params.dig(:sports_class, :category).present?
+      @sports_classes = @sports_classes.where(category: params[:sports_class][:category])
+    end
+    # Parameters: {"sports_class"=>{"difficulty_level"=>"1", "starts_at"=>"", "category"=>""}}
+    if params.dig(:sports_class, :difficulty_level).present?
+      @sports_classes = @sports_classes.where(difficulty_level: params[:sports_class][:difficulty_level])
+        if params.dig(:sports_class, :category, :difficulty_level).present?
+          @sports_classes = @sports_classes.where(category: params[:sports_class][:category], difficulty_level: params[:sports_class][:difficulty_level])
+        end
+    end
   end
 end
