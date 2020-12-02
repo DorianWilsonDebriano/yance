d749e80f808a0ce6d0bf6962f67ba9179818abfb
edoardo maraia <edoardo.maraia@gmail.com>
2020-12-02 17:31:40 +0100

    optimizing N+1

--- app/models/class_booking.rb
+++ app/models/class_booking.rb
@@ -1,6 +1,6 @@
 class ClassBooking < ApplicationRecord
-  belongs_to :user, touch: true
-  belongs_to :sports_class, touch: true
+  belongs_to :user
+  belongs_to :sports_class
 
   validates_uniqueness_of :user_id, :scope => [:sports_class_id]
 end
--- app/models/sports_class.rb
+++ app/models/sports_class.rb
@@ -1,8 +1,8 @@
 class SportsClass < ApplicationRecord
-  belongs_to :trainer, touch: true
+  belongs_to :trainer
   has_one(:user, through: :trainer)
   has_one_attached :photo
-  has_many :class_bookings, dependent: :destroy, touch: true
+  has_many :class_bookings
   has_many :users, through: :class_bookings
 
 
