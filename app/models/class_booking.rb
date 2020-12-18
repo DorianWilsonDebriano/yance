5be743876e9bc7d652ef4c8f46f6f47aa64d02e8
Cassandra Dodd <cassy.dodd95@gmail.com>
2020-12-18 11:36:38 +0000

    class_booking, sports_class, subscription and trainer models refactored

--- app/models/class_booking.rb
+++ app/models/class_booking.rb
@@ -1,7 +1,5 @@
 class ClassBooking < ApplicationRecord
   belongs_to :user
   belongs_to :sports_class
-
-  validates_uniqueness_of :user_id, :scope => [:sports_class_id]
+  validates_uniqueness_of :user_id, scope: [:sports_class_id]
 end
-
