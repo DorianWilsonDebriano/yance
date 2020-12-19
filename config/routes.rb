835ffa1bcc275eb6d3290c3ddd0aa46350a9b868
Dorian Wilson-Debriano <dw.debriano@gmail.com>
2020-12-19 17:40:35 +0100

    root for logged in users is now the index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    get "/", to: "sports_classes#index"
+  end
+
   root to: 'pages#home'
   get "/search", to: "pages#search", as: :search
   get "/profile", to: "pages#profile"
@@ -13,6 +18,7 @@ Rails.application.routes.draw do
   resources :sports_classes, only: %i[edit update destroy]
   resources :reviews, only: %i[edit update destroy]
 
+
   resources :sports_classes, only: %i[index show] do
     resources :class_bookings, only: %i[show new create]
   end
@@ -22,8 +28,6 @@ Rails.application.routes.draw do
   end
 
   resources :subscriptions, only: %i[edit update destroy]
-
-  # delete "booking/:id", to: "class_bookings#destroy"
 
   resources :class_bookings, only: %i[edit update destroy]
   get "/stream", to: "classes#stream"
