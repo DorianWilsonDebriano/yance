7e99b04d59a0871890178752965c94ee311d1af8
Dorian Wilson-Debriano <dw.debriano@gmail.com>
2020-12-20 20:34:09 +0100

    added a preview to the photo in the form + restyled the upload image button on sports_classes/new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,8 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import "../plugins/flatpickr";
 
+import { previewImageOnFileSelect } from "../components/photo_preview";
 import { initStarRating } from "../plugins/init_star_rating";
-
 // for confirmations
 // import { initSweetalert } from "../plugins/init_sweetalert";
 // for cancellations
@@ -38,54 +38,21 @@ document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
   initStarRating();
-  // initSweetalert("#sweet-alert-trainer", {
-  //   title: "Congratulations!",
-  //   text: "You're now a trainer",
-  //   icon: "success",
-  // });
+  previewImageOnFileSelect();
 
-  // initSweetalert("#sweet-alert-class", {
-  //   title: "Congratulations!",
-  //   text: "Your class has been created",
-  //   icon: "success",
-  // });
+  flatpickr(".datepicker", {
+  altInput: true,
+  altFormat: "d M Y",
+  dateFormat: "Z",
+  mode: "range"
+  });
 
-  // initSweetalertAll(
-  //   "#sweet-alert-booked",
-  //   {
-  //     title: "Congratulations!",
-  //     text: "Your class has been booked",
-  //     icon: "success",
-  //   },
-  //   (value) => {
-  //     console.log(value);
-  //     if (value) {
-  //       console.log(value, "hello");
-  //       const id = value.classList[0].split("-")[3];
-  //       console.log(id);
-  //       const link = document.getElementsByClassName(id);
-  //       console.log(link);
-  //       link[0].click();
-  //     }
-  //   }
-  // );
-
-  // initSweetalert("#sweet-alert-subscription", {
-  //   title: "Congratulations!",
-  //   text: "Your membership has been upgraded",
-  //   icon: "success",
-  // });
-
-  // initSweetalert(
-  //   "#sweet-alert-delete",
-  //   {
-  //     text: "You're about to cancel your booking. Are you sure?",
-  //     icon: "warning",
-  //     buttons: true,
-  //     dangerMode: true,
-  //   },
-  //   function () {
-  //     return this;
-  //   }
-  // );
+  flatpickr(".datetimepicker", {
+  altInput: true,
+  enableTime: true,
+  altFormat: "F j, Y, H:i",
+  dateFormat: "Z",
+  minDate: "today",
+  time_24hr: true
+  });
 });
