518c88f33d6eff289d197677ac173806ac62c8b1
Cassandra Dodd <cassy.dodd95@gmail.com>
2020-12-23 16:12:34 +0000

    user's upcoming classes no longer display past classes, earlier bugs fixed with cancellign classes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-
   end
 
   def search
@@ -10,27 +9,7 @@ class PagesController < ApplicationController
 
   def profile
     @user = current_user
-    handle_class_bookings
-    handle_created_classes
-  end
-
-  private
-
-  def handle_class_bookings
-    start_range = Time.zone.now.in_time_zone(Time.now.zone)
-    end_range = Time.zone.now.in_time_zone(Time.now.zone).advance(days: 30)
-    @user_bookings = SportsClass
-      .where(id: current_user.class_bookings.pluck(:sports_class_id))
-      .where(date_time: Range.new(start_range, end_range))
-      .order(date_time: :asc)
-  end
-
-  def handle_created_classes
-    start_range = Time.zone.now.in_time_zone(Time.now.zone)
-    end_range = Time.zone.now.in_time_zone(Time.now.zone).advance(days: 30)
-    @user_sports_classes = SportsClass
-      .where(id: current_user.sports_classes.pluck(:id))
-      .where(date_time: Range.new(start_range, end_range))
-      .order(date_time: :asc)
+    @user_bookings = current_user.class_bookings
+    @user_sports_classes = current_user.sports_classes.order(date_time: :asc)
   end
 end
