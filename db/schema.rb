a0d4255c531f923128f43fc1decd38524274e52d
edoardo maraia <edoardo.maraia@gmail.com>
2020-12-23 12:12:46 +0100

    added subscription status column to subscriptions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_102714) do
+ActiveRecord::Schema.define(version: 2020_12_23_111111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_102714) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "credits"
+    t.string "subscription_status", default: "incomplete"
     t.index ["membership_id"], name: "index_subscriptions_on_membership_id"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
