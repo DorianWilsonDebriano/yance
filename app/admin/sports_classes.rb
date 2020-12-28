71c7b0dd896c5a739018f7b80486f4779f961c99
Cassandra Dodd <cassy.dodd95@gmail.com>
2020-12-28 17:26:54 +0000

    style sheet for sports class index added, table for index started

--- app/admin/sports_classes.rb
+++ app/admin/sports_classes.rb
@@ -11,13 +11,30 @@ ActiveAdmin.register SportsClass do
                 class_booking_attributes: [:id, :user_id, :sports_class_id],
                 trainer_attributes: [:id, :bio, :sport_category, :city, :user_id]
 
-  index as: :grid do |sports_class|
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        link_to image_tag(sports_class.photo), admin_sports_class_path(sports_class)
-      end
+
+  index do
+    column "Sports Class" do |sports_class|
+      link_to sports_class.title, admin_sports_class_path(sports_class)
+    end
+    column "Trainer" do |sports_class|
+      sports_class.trainer.user.first_name
+      link_to sports_class.trainer.user.first_name, admin_trainer_sports_classes_path(trainer)
     end
   end
+# column "Title" do |post|
+#     link_to post.title, admin_post_path(post)
+#   end
+  # index as: :grid, columns: 3 do |sports_class|
+  #   columns do
+  #     row sports_class.title
+  #     div class: "grid_container" do
+  #       # span class: "image_container" do
+  #       #   sports_class.title
+  #       #   link_to image_tag(sports_class.photo), admin_sports_class_path(sports_class)
+  #       # end
+  #     end
+  #   end
+  # end
   # index do |sports_class|
   #   # attributes_table do
   #   #   row :id
