2bfb29e54e143ea02f690d1d5a49775c6b5b8a8b
Cassandra Dodd <cassy.dodd95@gmail.com>
2020-12-29 11:09:31 +0000

    user admin index and show working, with associated links

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -11,7 +11,8 @@ ActiveAdmin.register User do
                 :reset_password_token,
                 :reset_password_sent_at,
                 :remember_created_at,
-                :username, :first_name,
+                :username,
+                :first_name,
                 :last_name,
                 :bio,
                 :language,
@@ -19,20 +20,6 @@ ActiveAdmin.register User do
                 subscription_attributes: [:id, :user_id, :membership_id, :credits],
                 membership_attributes: [:id, :title, :credits, :price, :description],
                 trainer_attributes: [:id, :bio, :sport_category, :city, :user_id]
-
-
-  # index do
-  #   selectable_column
-  #   column :id
-  #   column :email
-  #   column :first_name
-  #   column :last_name
-  #   column :encrypted_password
-  #   column :created_at
-  #   column :trainer_ids
-  #   column :admin
-  #   actions
-  # end
 
   index do
     column "Name" do |user|
@@ -47,9 +34,69 @@ ActiveAdmin.register User do
     column "Photo" do |user|
       link_to image_tag(user.photo), admin_user_path(user)
     end
-    column "Trainer" do |user|
-      # user.trainer_ids
-      link_to user.trainer_ids, admin_trainer_path(user.trainer_ids)
+    column "Trainer ID" do |user|
+      link_to user.trainer_ids.join, admin_trainer_path(user.trainer_ids)
+    end
+    column "Lanuages" do |user|
+      user.language
+    end
+    column "Admin" do |user|
+      user.admin
+    end
+    column "Classes" do |user|
+      user_classes = SportsClass.find(user.sports_class_ids)
+      ul do
+        user_classes.each do |sports_class|
+          li link_to sports_class.title, admin_sports_class_path(sports_class)
+        end
+      end
     end
   end
+
+  show title: :first_name do
+    attributes_table do
+      row :profile_picture do |ad|
+        image_tag user.photo
+      end
+      row :first_name do
+        user.first_name
+      end
+      row :last_name do
+        user.last_name
+      end
+      row :user_created_on do |ad|
+        user.created_at
+      end
+      row :bio do
+        user.bio
+      end
+      row :encrypted_password do
+        user.encrypted_password
+      end
+      row :languages do
+        user.language
+      end
+      row :membership do
+        user_membership = Membership.find(Subscription.where(user_id: Subscription.pluck(:user_id)).first.membership_id)
+        link_to user_membership.title, admin_membership_path(user_membership)
+      end
+      row :Trainer_ID do
+        link_to user.trainer_ids.join, admin_trainer_path(user.trainer_ids)
+      end
+      row :authorised_admin do
+        user.admin
+      end
+    end
+    active_admin_comments
+  end
 end
+
+  # sidebar "Details", only: :show do
+  #   attributes_table_for book do
+  #     row :title
+  #     row :author
+  #     row :publisher
+  #     row('Published?') { |b| status_tag b.published? }
+  #   end
+  # end
+# end
