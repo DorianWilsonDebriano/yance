60aff09b78dfcc0761808c2a1c2e4827ac41d5f3
Cassandra Dodd <cassy.dodd95@gmail.com>
2020-12-30 16:46:13 +0000

    dashboard panels are now with display flex and in rows

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -13,25 +13,25 @@ ActiveAdmin.register_page "Dashboard" do
                 end
               end
             end
-            columns class: "dashboard-colums" do
-              column class: "dashboard-column" do
-                panel "Current Trainers" do
-                  ul do
-                    Trainer.limit(20).map do |trainer|
-                      li link_to(trainer.user.email, admin_trainers_path(trainer))
-                    end
-                  end
+          end
+        end
+        columns class: "dashboard-colums" do
+          column class: "dashboard-column" do
+            panel "Current Trainers" do
+              ul do
+                Trainer.limit(20).map do |trainer|
+                  li link_to(trainer.user.first_name, admin_trainers_path(trainer))
                 end
               end
             end
-            columns class: "dashboard-colums" do
-              column class: "dashboard-column" do
-                panel "Current Classes" do
-                  ul do
-                    SportsClass.limit(20).map do |sports_class|
-                      li link_to(sports_class.title, admin_sports_class_path(sports_class))
-                    end
-                  end
+          end
+        end
+        columns class: "dashboard-colums" do
+          column class: "dashboard-column" do
+            panel "Current Classes" do
+              ul do
+                SportsClass.limit(20).map do |sports_class|
+                  li link_to(sports_class.title, admin_sports_class_path(sports_class))
                 end
               end
             end
@@ -41,25 +41,3 @@ ActiveAdmin.register_page "Dashboard" do
     end
   end
 end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    # column do
-    #   panel "Current Users" do
-    #     ul do
-    #       User.limit(100).map do |user|
-    #         li link_to(user.email, admin_user_path(user))
-    #       end
-    #     end
-    #   end
-    # end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-      # end
-  #   end # content
-  # end
