ccc5f96d81900678bd5bfab784ceee632c0951c5
Dorian Wilson-Debriano <dw.debriano@gmail.com>
2020-12-30 22:20:31 +0100

    added password security on bookings w/o subscription

--- app/controllers/class_bookings_controller.rb
+++ app/controllers/class_bookings_controller.rb
@@ -9,27 +9,44 @@ class ClassBookingsController < ApplicationController
 
   def create
     @sports_class = SportsClass.find(params[:sports_class_id])
-    @classbooking = ClassBooking.new
+    @classbooking = ClassBooking.new(class_booking_params)
     @classbooking.user = current_user
     @classbooking.sports_class = @sports_class
     @subscription = current_user.subscription
     @membership = current_user.subscription.membership if current_user.subscription != nil
-    if @classbooking.save
-      @subscription.update(credits: @subscription.credits - 1) if current_user.subscription != nil
-      redirect_to sports_classes_path, notice: "Your class is successfully booked!"
+    if current_user.subscription.nil?
+      if check_password
+        @classbooking.save
+        redirect_to sports_classes_path, notice: "Your class is successfully booked!"
+      else
+        redirect_to sports_classes_path, notice: "wrong password for this class"
+      end
     else
-      redirect_to sports_classes_path, notice: "You have already booked this class"
+      if @classbooking.save
+        redirect_to sports_classes_path, notice: "Your class is successfully booked!"
+      else
+        redirect_to sports_classes_path, notice: "You have already booked this class"
+      end
     end
     authorize @classbooking
   end
 
   def destroy
     @classbooking = ClassBooking.find(params[:id])
-    @membership = current_user.subscription.membership
+    @membership = current_user.subscription.membership unless current_user.subscription.nil?
     @subscription = current_user.subscription
     @classbooking.destroy
-    @subscription.update(credits: @subscription.credits + 1)
     redirect_to sports_classes_path
     authorize @classbooking
+  end
+
+  private
+
+  def class_booking_params
+    params.require(:class_booking).permit(:password, user_attributes: [:id], sports_class_attributes: [:id])
+  end
+
+  def check_password
+    @classbooking.password == @sports_class.password
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_150317) do
+ActiveRecord::Schema.define(version: 2020_12_30_173421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_150317) do
     t.bigint "sports_class_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
     t.index ["sports_class_id"], name: "index_class_bookings_on_sports_class_id"
     t.index ["user_id"], name: "index_class_bookings_on_user_id"
   end
@@ -81,6 +82,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_150317) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "room"
+    t.string "password"
     t.index ["trainer_id"], name: "index_sports_classes_on_trainer_id"
   end
 
