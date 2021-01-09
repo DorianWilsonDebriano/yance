e04b0816eb8fd2a749c77a0409a45c06bc5c3569
edoardo maraia <edoardo.maraia@gmail.com>
2021-01-09 17:50:24 +0100

    working on subscription controller logic

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,11 +2,15 @@ class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @membership = Membership.find_by(title: params[:title])
-    @user = current_user
-    @subscription = Subscription.new
-    authorize @subscription
+    # @membership = Membership.find_by(title: params[:title])
+    # @user = current_user
+    # @subscription = Subscription.new
+    # authorize @subscription
   end
+
+  #when user hits choose, create method gets triggered, checks for params passed from
+  #form and if params are valid then creates checkout session, if checkout session
+  #is completed/successfull then we create a subscription on our database.
 
   def create
     @membership = Membership.find(params[:membership_id])
@@ -25,6 +29,45 @@ class SubscriptionsController < ApplicationController
       redirect_to memberships_path, notice: "You are alredy subscribed to this membership"
     end
   end
+
+  # def create
+  #   @membership = Membership.find(params[:membership_id])
+  #   @user = current_user
+  #   @subscription = Subscription.new
+  #   @subscription.user = current_user
+  #   @subscription.membership = @membership
+  #   authorize @subscription
+  #   customer = create_stripe_customer(@user)
+  #   @session = create_checkout_session(customer, @user)
+  #   session[:token] = @user.session_token
+  #   render :checkout
+  #   if @subscription.save
+  #     redirect_to root_path
+  #   else
+  #     @membership == current_user.membership
+  #     redirect_to memberships_path, notice: "You are alredy subscribed to this membership"
+  #   end
+  # end
+
+  # def create
+  #   @user = current_user
+  #   @membership = Membership.find(params[:membership_id])
+  #   @customer = create_stripe_customer(@user)
+  #   @session = create_checkout_session(@customer, @user)
+  #   session[:token] = @user.session_token
+  #   render :checkout
+  #   if customer_subscription?
+  #     @membership = Membership.find(params[:membership_id])
+  #     @user = current_user
+  #     @subscription = Subscription.new
+  #     @subscription.user = current_user
+  #     @subscription.membership = @membership
+  #     authorize @subscription
+  #   else
+  #     @membership == current_user.membership
+  #     redirect_to memberships_path, notice: "You are alredy subscribed to this membership"
+  #   end
+  # end
 
   def edit
   end
@@ -49,21 +92,33 @@ class SubscriptionsController < ApplicationController
   end
 
   def create_checkout_session(customer, user)
-    price = Stripe::Price.list(lookup_keys: [current_user.membership.title]).data.first
+    price = Stripe::Price.list(lookup_keys: [@membership.title]).data.first
 
-    Stripe::Checkout::Session.create({
-      customer: current_user.stripe_customer_id,
-      success_url: 'http://localhost:3000/',
-      cancel_url: 'http://localhost:3000/memberships',
-      payment_method_types: ['card'],
-      line_items: [{
-        price: price.id,
-        quantity: 1,
-      }],
-      mode: 'subscription',
-    })
+    @checkout_session = Stripe::Checkout::Session.create({
+        customer: current_user.stripe_customer_id,
+        success_url: 'http://localhost:3000/',
+        cancel_url: 'http://localhost:3000/memberships',
+        payment_method_types: ['card'],
+        line_items: [{
+          price: price.id,
+          quantity: 1,
+        }],
+        mode: 'subscription',
+      })
     # session = event.data.object
     # @subscription = Subscription.find_by(user_id: current_user.id)
     # @subscription.update!(stripe_id: session.checkout.session.subscription)
   end
+
+  # def customer_subscription?
+  #   customer = Stripe::Customer.retrieve(
+  #   subscription.customer
+  # )
+  #   subscription = Stripe::Subscription.retrieve(
+  #   subscription.customer
+  # )
+  #   !customer.first.empty?
+  # end
 end
+
+
