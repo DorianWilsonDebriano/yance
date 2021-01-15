d9456a261c3af4a088491f9f77492a5051a7f0d5
Dorian Wilson-Debriano <dw.debriano@gmail.com>
2021-01-15 14:34:51 +0100

    fixed trainer show bug

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,5 +1,5 @@
 class TrainersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :show
   before_action :set_trainer, only: [:show, :edit, :update, :destroy]
 
   def show
