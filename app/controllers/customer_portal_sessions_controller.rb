b0e7226061fd367852e44c3f4437cacb50f29148
edoardo maraia <edoardo.maraia@gmail.com>
2021-01-26 16:47:49 +0100

    changed redirect url of customer portal controller

--- app/controllers/customer_portal_sessions_controller.rb
+++ app/controllers/customer_portal_sessions_controller.rb
@@ -1,11 +1,10 @@
 class CustomerPortalSessionsController < ApplicationController
   before_action :skip_authorization, only: [:create]
   def create
-    return_URL = Rails.env.development? ? "http://localhost:3000/settings" : "https://yancesport/settings"
+    return_url = Rails.env.development? ? "http://localhost:3000/settings" : "https://yancesport/settings"
     portal_session = Stripe::BillingPortal::Session.create({
       customer: current_user.stripe_customer_id,
-      #laura sample user id 'cus_Ia6uu8xXbZUSPw'
-      return_url: return_URL,
+      return_url: return_url
     })
     redirect_to portal_session.url
   end
