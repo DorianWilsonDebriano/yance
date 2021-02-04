cccc7b5fa26c12905171ff47daeef41c175da1e1
Dorian Wilson-Debriano <dw.debriano@gmail.com>
2021-02-04 19:19:28 +0100

    en locale up to trainers show

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # translations to configure
+  config.i18n.available_locales = [:de, :en]
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
