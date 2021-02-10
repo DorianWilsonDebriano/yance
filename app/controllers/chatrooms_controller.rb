72859b40d967734a9bfc61038cb3f6d014e7231f
Cassandra Dodd <cassy.dodd95@gmail.com>
2021-02-10 16:41:01 +0100

    message time and date moved above, styling of community chatrooms index page started

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,10 +3,6 @@ class ChatroomsController < ApplicationController
   def index
     @chatrooms = policy_scope(Chatroom)
     @user = current_user
-    @user_bookings = @user.all_booked_classes.pluck(:title)
-    @chatroom_names = @chatrooms.pluck(:name)
-    @user_class_chatrooms = @user_bookings & @chatroom_names
-    class_chatrooms
     authorize @chatrooms
   end
 
@@ -15,27 +11,6 @@ class ChatroomsController < ApplicationController
     @message = Message.new
     authorize @chatroom
   end
-
-#   def create
-#     @chatroom = Chatroom.new(chatroom_params)
-#     if @chatroom.save
-#       redirect_to chatrooms_path
-#     else
-#       render '/profile'
-#     end
-#   end
-
-  private
-
-  def class_chatrooms
-    if !@user_class_chatrooms.empty?
-      @chatrooms_all = Chatroom.where(name: @user_class_chatrooms)
-    end
-  end
-
-#   def chatroom_params
-#     params.require(:chatroom).permit(:name)
-#   end
 end
 
 
