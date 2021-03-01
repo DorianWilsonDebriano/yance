273ac5823da534334edd79ee493b21dc342389a2
Cassandra Dodd <cassy.dodd95@gmail.com>
2021-03-01 13:04:03 +0100

    trainers chatroom is now params protected

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomsController < ApplicationController
-  skip_before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @chatrooms = policy_scope(Chatroom)
     @user = current_user
@@ -7,11 +8,24 @@ class ChatroomsController < ApplicationController
   end
 
   def show
-    @chatroom = Chatroom.find(params[:id])
-    @message = Message.new
+    if params[:id] == Chatroom.second.id.to_s
+      enforce_tenancy_trainers
+      @chatroom = Chatroom.find(params[:id])
+      @message = Message.new
+    else
+      @chatroom = Chatroom.find(params[:id])
+      @message = Message.new
+    end
     authorize @chatroom
   end
+
+  private
+
+  def current_user_trainer?
+    !current_user.trainer_ids.empty?
+  end
+
+  def enforce_tenancy_trainers
+    redirect_to chatrooms_path, notice: "You don't have access to this chatroom." unless current_user_trainer?
+  end
 end
-
-
-
